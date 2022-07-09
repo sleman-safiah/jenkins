92565a229604dfc19118e0b2dc366371fa7e34d8
sleman-safiah <105430699+sleman-safiah@users.noreply.github.com>
2022-07-09 12:06:57 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & sdasdsdfsdfads
+          Learn React & sdasds
         </a>
       </header>
     </div>
