1351669da1c6e78f5b17c44fc63c5c3762d76cf5
Sleman Safiah <148073@appareluae.com>
2022-07-09 11:56:19 +0400

    add yml file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & sd asdasdads
+          Learn React & sdasdads
         </a>
       </header>
     </div>
