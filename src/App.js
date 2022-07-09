a1e172c51f81ce4cdbb7b1c0175758b7743aa94f
Sleman Safiah <148073@appareluae.com>
2022-07-09 12:11:37 +0400

    asd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & sdasdasdasds
+          Learn React & sdasdsdfsdfsdfbbbbbbb
         </a>
       </header>
     </div>
